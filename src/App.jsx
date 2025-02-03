455f43f80d60d8255f3dea8f82944d221a162360
Andrew Vasquez <103331877+Allials@users.noreply.github.com>
2025-02-03 09:52:42 -0600

    Initali Commit with react and tailwind dependencies

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='min-h-screen bg-gray-500 flex items-center justify-center'>
+          
+          <h1>Todo App</h1>
+        
+        </div>
+    </>
+  )
+}
+
+export default App
